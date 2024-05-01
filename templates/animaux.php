a8db4d9bc3ca77941e676844f3a835e2a7d3890d
DeRetourDuFutur <antonymasson.dev@gmail.com>
2024-05-01 22:56:04 +0200

    MAJ Animaux

--- templates/animaux.php
+++ templates/animaux.php
@@ -3,95 +3,96 @@
 <div class="container-fluid animals-banner" id="animaux">
   <!-- Bannière ANIMAUX | Début -->
   <div class="py-5"></div>
-  <!-- Bannière ANIMAUX | Fin -->  
+  <!-- Bannière ANIMAUX | Fin -->
 </div>
 <!-- Contenu ANIMAUX | Début -->
-<div class="animals-content">
-  <p>
-    <span class="text-primary me-2">#</span>Nos animaux
-  </p>
-  <h1>Rencontrez nos animaux <span class="text-primary">attachants</span></h1>
-  <p>
-    <div class="slider-animals">
-      <!-- Liste Items | Début -->
-      <div class="list-slider-animals">
-        <!-- Slider Savane | Début -->
-        <div class="item-slider-animals">
-          <img src="assets/img/animals/covers/arc-sav-cover-slider.webp" alt="La Savane" />
-          <div class="content-slider-animals">
-            <div class="author-slider-animals">ARCADIA</div>
-            <div class="topic-slider-animals">SAVANE</div>
-            <div class="des-slider-animals">
-              Décourvrez notre domaine entièrement dédié à la faune et la flore de la Savane
-            </div>
+<div class="mt-5">
+  <div class="container-sm">
+    <p>
+      <span class="text-primary me-2">#</span>Nos animaux
+    </p>
+    <h1>Rencontrez nos animaux <span class="text-primary">attachants</span></h1>
+  </div>
+  <div class="slider-animals">
+    <!-- Liste Items | Début -->
+    <div class="list-slider-animals">
+      <!-- Slider Savane | Début -->
+      <div class="item-slider-animals">
+        <img src="assets/img/animals/covers/arc-sav-cover-slider.webp" alt="La Savane" />
+        <div class="content-slider-animals">
+          <div class="author-slider-animals">ARCADIA</div>
+          <div class="topic-slider-animals">SAVANE</div>
+          <div class="des-slider-animals">
+            Décourvrez notre domaine entièrement dédié à la faune et la flore de la Savane
+          </div>
+          <div class="buttons-slider-animals">
+            <button>Explorer</button>
+          </div>
+        </div>
+      </div>
+      <!-- Slider Savane | Fin -->
+      <!-- Slider Jungle | Début -->
+      <div class="item-slider-animals">
+        <img src="assets/img/animals/covers/arc-jun-cover-slider.webp" alt="La Jungle" />
+        <div class="content-slider-animals">
+          <div class="author-slider-animals">ARCADIA</div>
+          <div class="topic-slider-animals">JUNGLE</div>
+          <div class="des-slider-animals">
+            Décourvrez notre domaine entièrement dédié à la faune et la flore de la Jungle
+          </div>
+          <div class="buttons-slider-animals">
+            <button>Explorer</button>
+          </div>
+        </div>
+      </div>
+      <!-- Slider Jungle | Fin -->
+      <!-- Slider Marais | Début -->
+      <div class="item-slider-animals">
+        <img src="assets/img/animals/covers/arc-swa-cover-slider.webp" alt="Les Marais" />
+        <div class="content-slider-animals">
+          <div class="author-slider-animals">ARCADIA</div>
+          <div class="topic-slider-animals">MARAIS</div>
+          <div class="des-slider-animals">
+            Décourvrez notre domaine entièrement dédié à la faune et la flore des Marais
             <div class="buttons-slider-animals">
               <button>Explorer</button>
             </div>
           </div>
         </div>
-        <!-- Slider Savane | Fin -->
-        <!-- Slider Jungle | Début -->
-        <div class="item-slider-animals">
-          <img src="assets/img/animals/covers/arc-jun-cover-slider.webp" alt="La Jungle" />
-          <div class="content-slider-animals">
-            <div class="author-slider-animals">ARCADIA</div>
-            <div class="topic-slider-animals">JUNGLE</div>
-            <div class="des-slider-animals">
-              Décourvrez notre domaine entièrement dédié à la faune et la flore de la Jungle
-            </div>
-            <div class="buttons-slider-animals">
-              <button>Explorer</button>
-            </div>
-          </div>
-        </div>
-        <!-- Slider Jungle | Fin -->
-        <!-- Slider Marais | Début -->
-        <div class="item-slider-animals">
-          <img src="assets/img/animals/covers/arc-swa-cover-slider.webp" alt="Les Marais" />
-          <div class="content-slider-animals">
-            <div class="author-slider-animals">ARCADIA</div>
-            <div class="topic-slider-animals">MARAIS</div>
-            <div class="des-slider-animals">
-              Décourvrez notre domaine entièrement dédié à la faune et la flore des Marais
-              <div class="buttons-slider-animals">
-                <button>Explorer</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Slider Marais | Fin -->
       </div>
-      <!-- Liste Items | Fin -->
-      <!-- Liste Thumbnail | Début -->
-      <div class="thumbnail-slider-animals">
-        <div class="item-slider-animals">
-          <img src="assets/img/animals/covers/arc-sav-cover-card.jpg" alt="La Savane" />
-          <div class="content-slider-animals">
-            <div class="title-slider-animals">Savane</div>
-          </div>
-        </div>
-        <div class="item-slider-animals">
-          <img src="assets/img/animals/covers/arc-jun-cover-card.jpg" alt="La Jungle" />
-          <div class="content-slider-animals">
-            <div class="title-slider-animals">Jungle</div>
-          </div>
-        </div>
-        <div class="item-slider-animals">
-          <img src="assets/img/animals/covers/arc-swa-cover-card.jpg" alt="Les Marais" />
-          <div class="content-slider-animals">
-            <div class="title-slider-animals">Marais</div>
-          </div>
-        </div>
-      </div>
-      <!-- Liste Thumbnail | Fin -->
-      <!-- Boutons NEXT / PREV -->
-      <div class="arrows-slider-animals">
-        <button id="prev" class="btn-prev"><i class="fa-solid fa-hippo fa-flip-horizontal prev-hippo"></i></button>
-        <button id="next" class="btn-next"><i class="fa-solid fa-hippo next-hippo"></i></button>
-      </div>
-      <!-- Time Running -->
-      <div class="time-slider-animals"></div>
+      <!-- Slider Marais | Fin -->
     </div>
+    <!-- Liste Items | Fin -->
+    <!-- Liste Thumbnail | Début -->
+    <div class="thumbnail-slider-animals">
+      <div class="item-slider-animals">
+        <img src="assets/img/animals/covers/arc-sav-cover-card.jpg" alt="La Savane" />
+        <div class="content-slider-animals">
+          <div class="title-slider-animals">Savane</div>
+        </div>
+      </div>
+      <div class="item-slider-animals">
+        <img src="assets/img/animals/covers/arc-jun-cover-card.jpg" alt="La Jungle" />
+        <div class="content-slider-animals">
+          <div class="title-slider-animals">Jungle</div>
+        </div>
+      </div>
+      <div class="item-slider-animals">
+        <img src="assets/img/animals/covers/arc-swa-cover-card.jpg" alt="Les Marais" />
+        <div class="content-slider-animals">
+          <div class="title-slider-animals">Marais</div>
+        </div>
+      </div>
+    </div>
+    <!-- Liste Thumbnail | Fin -->
+    <!-- Boutons NEXT / PREV -->
+    <div class="arrows-slider-animals">
+      <button id="prev" class="btn-prev"><i class="fa-solid fa-hippo fa-flip-horizontal prev-hippo"></i></button>
+      <button id="next" class="btn-next"><i class="fa-solid fa-hippo next-hippo"></i></button>
+    </div>
+    <!-- Time Running -->
+    <div class="time-slider-animals"></div>
+  </div>
   </p>
 </div>
 <!-- Contenu ANIMAUX | Fin -->
