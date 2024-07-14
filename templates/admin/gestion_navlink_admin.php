6bd28d4a63b984bd340d93d42d413d0aaece5395
DeRetourDuFutur <antonymasson.dev@gmail.com>
2024-07-15 01:04:03 +0200

    MAJ NAVBAR ADMIN

--- templates/admin/gestion_navlink_admin.php
+++ templates/admin/gestion_navlink_admin.php
@@ -26,6 +26,35 @@ require_once '../app/admin/gestion_navlink_admin.php';
               <label for="navlink_admin_ico" class="ms-1">ICO</label>
               <input type="text" class="form-control" id="navlink_admin_ico" name="navlink_admin_ico" value="<?= $navlink_admin['navlink_admin_ico']; ?>">
             </div>
+            <div class="alert alert-secondary my-0">
+              <label for="navlink_admin_class" class="ms-1">CLASSE LIENS</label>
+              <input type="text" class="form-control" id="navlink_admin_class" name="navlink_admin_class" value="<?= $navlink_admin['navlink_admin_class']; ?>">
+            </div>
+            <div class="alert alert-secondary my-0">
+              ACC&Egrave;S AUX MENUS
+              <div class="input-group mb-3">
+                <label class="input-group-text <?php if ($navlink_admin['navlink_admin_a'] == '1') echo 'text-primary'; ?> <?php if ($navlink_admin['navlink_admin_a'] == '0') echo 'text-secondary'; ?>" for="navlink_admin_a">ADM.</label>
+                <select class="form-select" id="navlink_admin_a" name="navlink_admin_a">
+                  <option value="1" <?php if ($navlink_admin['navlink_admin_a'] == '1') echo 'selected'; ?> class="text-primary fw-bold">ACTIF</option>
+                  <option value="0" <?php if ($navlink_admin['navlink_admin_a'] == '0') echo 'selected'; ?> class="text-secondary fw-bold">INACTIF</option>
+                </select>
+              </div>
+              <div class="input-group mb-3">
+                <label class="input-group-text <?php if ($navlink_admin['navlink_admin_e'] == '1') echo 'text-primary'; ?> <?php if ($navlink_admin['navlink_admin_e'] == '0') echo 'text-secondary'; ?>" for="navlink_admin_e">EMP.</label>
+                <select class="form-select" id="navlink_admin_e" name="navlink_admin_e">
+                  <option value="1" <?php if ($navlink_admin['navlink_admin_e'] == '1') echo 'selected'; ?> class="text-primary fw-bold">ACTIF</option>
+                  <option value="0" <?php if ($navlink_admin['navlink_admin_e'] == '0') echo 'selected'; ?> class="text-secondary fw-bold">INACTIF</option>
+                </select>
+              </div>
+              <div class="input-group mb-3">
+                <label class="input-group-text <?php if ($navlink_admin['navlink_admin_v'] == '1') echo 'text-primary'; ?> <?php if ($navlink_admin['navlink_admin_v'] == '0') echo 'text-secondary'; ?>" for="navlink_admin_v">V&Eacute;T.</label>
+                <select class="form-select" id="navlink_admin_v" name="navlink_admin_v">
+                  <option value="1" <?php if ($navlink_admin['navlink_admin_v'] == '1') echo 'selected'; ?> class="text-primary fw-bold">ACTIF</option>
+                  <option value="0" <?php if ($navlink_admin['navlink_admin_v'] == '0') echo 'selected'; ?> class="text-secondary fw-bold">INACTIF</option>
+                </select>
+              </div>
+            </div>
+
             <button type="submit" class="btn btn-primary-color align-bottom mt-4" name="navlink_admin_action" value="update">MAJ</button>
           </form>
         </div>
